4c6f68fbac76fc6393787ef8fe426525d9f15e24
Michał Cieślik <60623457+Michalcieslik1@users.noreply.github.com>
2022-09-29 17:51:22 -0400

    Finished switching to MVVM with Core Data! Adding and removing reeds work, with changes updating in every view. List view models created for reed and ReedBox entities. Deleting reed boxes is currently impossible, need to figure out a way to use .onDelete() with a custom button

---
+++ Core Data Tutorial/Core Data Tutorial/Core_Data_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Data_TutorialApp.swift
+//  Core Data Tutorial
+//
+//  Created by Michał Cieslik on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Core_Data_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
